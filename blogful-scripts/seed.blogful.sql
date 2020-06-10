47247179736c290d76021f88d15d6ab94f757eec
S. Steven Reyes <s.steven.reyes@gmail.com>
2020-06-10 15:29:32 -0400

    added shopping-list for assignment

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO articles (title, content)
+VALUES
+  ('Title 1', 'My content 1'),
+  ('Title 2', 'My content 2'),
+  ('Title 3', 'My content 3'),
+  ('Title 4', 'My content 4'),
+  ('Title 5', 'My content 5'),
+  ('Title 6', 'My content 6'),
+  ('Title 7', 'My content 7'),
+  ('Title 8', 'My content 8'),
+  ('Title 9', 'My content 9'),
+  ('Title 10', 'My content 10'),
+  ('Title 11', 'My content 11'),
+  ('Title 12', 'My content 12'),
+  ('Title 13', 'My content 13'),
+  ('Title 14', 'My content 14'),
+  ('Title 15', 'My content 15'),
+  ('Title 16', 'My content 16'),
+  ('Title 17', 'My content 17'),
+  ('Title 18', 'My content 18'),
+  ('Title 19', 'My content 19'),
+  ('Title 20', 'My content 20')
+;
